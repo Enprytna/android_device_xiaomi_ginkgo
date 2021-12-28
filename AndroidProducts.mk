6e6dc6f0d049d1d939c10fe87f8dc80abbe5804b
Enprytna <e.endipriyatna@gmail.com>
2021-12-28 17:51:33 +0000

    ginkgo: ProjectStreak-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,14 @@
 #
 # Copyright (C) 2020 ArrowOS
+# Copyright (C) 2021 ProjectStreak
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
+    $(LOCAL_DIR)/streak_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    streak_ginkgo-user \
+    streak_ginkgo-userdebug \
+    streak_ginkgo-eng
