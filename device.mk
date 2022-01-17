e9b196300a8cb4544d6ac04998c0c449ee20d6cc
notakbuzz <bistodroid@gmail.com>
2022-01-17 11:41:36 +0000

    ginkgo: product soong for posp

--- device.mk
+++ device.mk
@@ -399,6 +399,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
 
+PRODUCT_BOARD_PLATFORM := trinket
+PRODUCT_USES_QCOM_HARDWARE := true
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
