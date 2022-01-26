cf60b969ec47a476e2675bf912ed1c8978b945f8
Enprytna <e.endipriyatna@gmail.com>
2022-01-26 09:30:18 +0000

    ginkgo: Project Radiant bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,14 @@
 #
 # Copyright (C) 2020 ArrowOS
+# Copyright (C) 2022 Project Radiant
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
+    $(LOCAL_DIR)/radiant_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    radiant_ginkgo-user \
+    radiant_ginkgo-userdebug \
+    radiant_ginkgo-eng
