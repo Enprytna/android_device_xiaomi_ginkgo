f65b34310f7c896400e89aeca0003db544e14875
Enprytna <e.endipriyatna@gmail.com>
2022-01-31 06:44:43 +0000

    ginkgo: Krypton-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
+    $(LOCAL_DIR)/krypton_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    krypton_ginkgo-user \
+    krypton_ginkgo-userdebug \
+    krypton_ginkgo-eng
