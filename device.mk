a13c15e3fd020c289ce56e1313d0b3b840be37c5
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-07-09 15:53:48 +0700

    ginkgo: perf: Import perfconfigstore from BSP
    Signed-off-by: Enprytna <e.endipriyatna@gmail.com>

--- device.mk
+++ device.mk
@@ -322,7 +322,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
-    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
 
 # QTI VNDK Framework Detect
 PRODUCT_PACKAGES += \
